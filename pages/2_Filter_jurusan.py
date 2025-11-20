a40c15cf880765ae32c022312668e1c7f50bdfb2
Luthfan05 <132749783+Luthfan05@users.noreply.github.com>
2025-11-20 15:14:28 +0700

    Add files via upload

---
+++ pages/2_Filter_jurusan.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Cari Berdasarkan Jurusan")
+
+st.info("Fitur ini masih dalam tahap pengembangan.")
+
+st.write("""
+Fitur pencarian berdasarkan jurusan akan ditambahkan pada update berikutnya.
+""")
